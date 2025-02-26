45fd4103a0bf4baddd85badcd9ae604eae09ec6e
Gilberto <contato.junior272005@gmail.com>
2025-02-26 20:48:54 -0300

    Segundo commit para atualizações

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int array[] = new int[10];
+
+        System.out.println("Informe numeros de 1 a 10:");
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print("Informe numero da posição " + (i + 1) + ": ");
+
+
+
+        }
+    }
+}
